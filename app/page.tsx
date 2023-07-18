01fea38b6fda3ce149396a3b4d06a05bc9fd893e
Nicole Ip <60815937+nicole-ip@users.noreply.github.com>
2023-07-17 20:11:27 -0400

    Styling vote page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import { createClient } from "hedsvote";
 
 async function getProposals() {
@@ -21,45 +22,104 @@ async function getAllTapes() {
 export default async function Home() {
   const proposals = await getProposals();
   const allTapes = await getAllTapes();
+  const mockProposals = [
+    {
+      id: 1,
+      image:
+        "https://www.heds.cloud/ipfs/QmceLhYvjioGowYT7EMtofiaWt7aYRrPbE4tLn8HjfZpyT",
+      name: "hedstape 16",
+      created_by: "heds",
+      timeline: "OPEN",
+    },
+    {
+      id: 2,
+      image:
+        "https://www.heds.cloud/ipfs/QmceLhYvjioGowYT7EMtofiaWt7aYRrPbE4tLn8HjfZpyT",
+      name: "reflections",
+      created_by: "daniel allen",
+      timeline: "OPEN",
+    },
+    {
+      id: 3,
+      image:
+        "https://www.heds.cloud/ipfs/QmceLhYvjioGowYT7EMtofiaWt7aYRrPbE4tLn8HjfZpyT",
+      name: "high frequency",
+      created_by: "noise",
+      timeline: "OPEN",
+    },
+    {
+      id: 4,
+      image:
+        "https://www.heds.cloud/ipfs/QmceLhYvjioGowYT7EMtofiaWt7aYRrPbE4tLn8HjfZpyT",
+      name: "heds solo",
+      created_by: "heds",
+      timeline: "CLOSED",
+    },
+  ];
+
   console.log("proposals", proposals);
   console.log("allTapes", allTapes);
 
   return (
     <main className="flex min-h-screen flex-col items-center">
-      <div className="p-12 w-2/3">
-        <div className="flex flex-row justify-between items-center">
+      <div className="w-2/3 p-12">
+        <div className="flex flex-row items-center justify-between">
           <p className="text-8xl font-bold">VOTING FOR</p>
           <Link href="/create-proposal">
             <div className="relative w-80">
-              <button className="flex items-center bg-transparent border-2 hover:bg-blue-600 text-white font-light py-4 px-6 rounded-full">
+              <button className="flex items-center rounded-full border-2 bg-transparent px-6 py-4 font-light text-white hover:bg-blue-600">
                 <span className="mr-10">CREATE A PROPOSAL</span>
               </button>
-              <div className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-white rounded-full h-10 w-28 flex items-center justify-center transition-all duration-300">
-                <span className="text-blue-500 text-lg">&#8594;</span>
+              <div className="absolute right-0 top-1/2 flex h-10 w-28 -translate-y-1/2 transform items-center justify-center rounded-full bg-white transition-all duration-300">
+                <span className="text-lg text-blue-500">&#8594;</span>
               </div>
             </div>
           </Link>
         </div>
-        <p className="text-8xl text-right font-bold mt-4">THE MASSES</p>
-      </div>
-      <div className="flex flex-col items-center bg-zinc-50 w-full text-indigo-900">
-        <div className="p-12 w-2/3">
-          <Link href="/proposals">
-            <p className="text-4xl">ALL PROPOSALS</p>
-          </Link>
-          {/* <div className="flex flex-row">
-            {proposals &&
-              proposals.map((proposal) => (
-                <div className="border-2 rounded-md" key={proposal.ipfs_hash}>
-                  <Link href={`/proposals/${proposal.ipfs_hash}`}>
-                    <p>{proposal.title}</p>
-                  </Link>
-                </div>
-              ))}
-          </div> */}
+        <p className="mt-4 text-right text-8xl font-bold">THE MASSES</p>
+        <div className="float-right mt-6 w-1/2">
+          <p className="text-right text-white">
+            Enhance Your Experience: Vote and Influence Your Favorite Tunes with
+            Our Cutting-Edge Media Voting Protocol
+          </p>
         </div>
       </div>
-      <div className="p-12 w-2/3">
+      <div className="flex w-full flex-col items-center bg-zinc-50">
+        <div className="w-2/3 p-12">
+          <Link href="/proposals">
+            <p className="text-4xl text-[#2D2934]">ALL PROPOSALS</p>
+          </Link>
+          <div className="flex flex-row justify-between">
+            {mockProposals &&
+              mockProposals.map((proposal) => (
+                // <div className="border-2 rounded-md" key={proposal.ipfs_hash}>
+                //   <Link href={`/proposals/${proposal.ipfs_hash}`}>
+                //     <p>{proposal.title}</p>
+                //   </Link>
+                // </div>
+
+                <div
+                  className="mt-8 box-border flex w-56 flex-col items-center justify-between rounded-2xl border-4 border-black p-4"
+                  key={proposal.id}>
+                  <div className="fit-content self-start rounded-2xl border-2	border-black px-3 text-center text-black">
+                    {proposal.timeline}
+                  </div>
+                  <Image
+                    className="rounded-full border-2 border-black"
+                    alt="Proposal cover image"
+                    src={proposal.image}
+                    width={75}
+                    height={75}
+                  />
+                  <p className="font-mono text-black">{proposal.name}</p>
+                  <p className="text-black">CREATED BY</p>
+                  <p className="text-black">{proposal.created_by}</p>
+                </div>
+              ))}
+          </div>
+        </div>
+      </div>
+      <div className="w-2/3 p-12">
         <p className="text-4xl text-white">YOU VOTED ON</p>
       </div>
     </main>
--- app/proposals/page.tsx
+++ app/proposals/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import { createClient } from "hedsvote";
 
 async function getProposals() {
@@ -12,19 +13,29 @@ async function getProposals() {
 
 export default async function Page() {
   const proposals = await getProposals();
+  console.log("proposals", proposals);
   return (
-    <div className="p-12">
-      <h1>All proposals</h1>
-      <div className="grid gap-2 grid-cols-6">
-        {proposals &&
-          proposals.map((proposal) => (
-            <div className="border-2 rounded-md" key={proposal.ipfs_hash}>
-              <Link href={`/proposals/${proposal.ipfs_hash}`}>
-                <p>{proposal.title}</p>
-              </Link>
+    <div className="flex flex-wrap p-12">
+      {proposals &&
+        proposals.map((proposal) => (
+          <Link
+            className="p-0.5"
+            href={`/proposals/${proposal.ipfs_hash}`}
+            key={proposal.ipfs_hash}>
+            <div className="flex flex-col items-center justify-between rounded-2xl border-2 border-black p-2">
+              <Image
+                alt="Proposal cover image"
+                className="rounded-md"
+                src={
+                  "https://www.heds.cloud/ipfs/QmceLhYvjioGowYT7EMtofiaWt7aYRrPbE4tLn8HjfZpyT"
+                }
+                width={300}
+                height={300}
+              />
+              <p className="text-mono w-full p-1 text-left">{proposal.title}</p>
             </div>
-          ))}
-      </div>
+          </Link>
+        ))}
     </div>
   );
 }
