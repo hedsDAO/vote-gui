27b2af5c091a6afe08870983432d5dee4c76f096
cgreer011 <camgreer01@gmail.com>
2023-08-02 18:50:54 -0500

    prisma integration completed

--- app/_actions.ts
+++ app/_actions.ts
@@ -1,6 +1,7 @@
 'use server'
 
 import axios from "axios";
+import { PrismaClient } from '@prisma/client'
 
 export const pinFileToIpfs = async (formData: any) => {
     return await axios.post("https://api.pinata.cloud/pinning/pinFileToIPFS", formData, {
@@ -10,4 +11,18 @@ export const pinFileToIpfs = async (formData: any) => {
           pinata_secret_api_key: process.env.PINATA_API_SECRET,
         },
     });
+}
+
+const prisma = new PrismaClient()
+
+export async function main() {
+  try {
+  const allUsers = await prisma.users.findMany()
+  console.log(allUsers)
+  await prisma.$disconnect()
+  } catch(e) {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  }
 }
