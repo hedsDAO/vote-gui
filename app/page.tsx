e7b9587022daf71d43773ff692199f6165451ef3
cgreer011 <camgreer01@gmail.com>
2023-08-03 11:34:45 -0500

    get userData for connected user

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,21 @@ import Image from "next/image";
 import { Web3Button } from "@web3modal/react";
 
 import { useAccount } from "wagmi";
-import { main } from "./_actions";
-import { useEffect } from "react";
+import { getuserData } from "./_actions";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const { isConnected } = useAccount();
+  const { address, isConnected } = useAccount();
+  const [userData, setUserData] = useState();
 
   console.log("isConnected", isConnected);
-  const testDbLoad = async () => {
-    main()
-  }
+  console.log("user data", userData);
   useEffect(() => {
-    testDbLoad();
-  },[])
+    if (!address) return;
+    getuserData(address).then((result) => {
+      setUserData(result);
+    })
+  },[address])
 
   const mockProposals = [
     {
