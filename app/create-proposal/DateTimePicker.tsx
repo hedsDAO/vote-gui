b10a71c40d4cb42065a1f9d1decd09cfca386bf8
cgreer011 <camgreer01@gmail.com>
2023-08-08 08:37:10 -0500

    datepicker flow + context complete

--- app/create-proposal/DateTimePicker.tsx
+++ app/create-proposal/DateTimePicker.tsx
@@ -8,14 +8,31 @@ interface OwnProps {
   minDate: Date;
 }
 
-const DateTimePicker = ({ startDate, setStartDate, minDate }: OwnProps) => {
+const DateTimePicker = ({ startDate, setStartDate, minDate}: OwnProps) => {
+  const currentTime = new Date();
+  let minTime: Date | undefined;
+  let maxTime: Date | undefined;
+
+  // Check if the selected date is the current date
+  if (startDate.toDateString() === currentTime.toDateString()) {
+    minTime = currentTime;
+    maxTime = new Date(currentTime);
+    maxTime.setHours(23, 59, 59, 999);
+  }
+
   return (
     <DatePicker
-      showTimeSelect
+     className="text-black"
       selected={startDate}
-      onChange={(date) => setStartDate(date)}
-      minDate={minDate}
+      onChange={(date) => date && setStartDate(date)}
+      showTimeSelect
+      timeFormat="HH:mm"
+      timeIntervals={15}
+      timeCaption="time"
       dateFormat="MMMM d, yyyy h:mm aa"
+      minDate={minDate}
+      minTime={minTime}
+      maxTime={maxTime}
     />
   );
 };
--- app/create-proposal/TimelineForm.tsx
+++ app/create-proposal/TimelineForm.tsx
@@ -1,13 +1,23 @@
 "use client";
-import { useState, ChangeEvent } from "react";
+import { useContext } from "react";
 import CustomFormInput from "./CustomFormInput";
 import DateTimePicker from "./DateTimePicker";
+import NextStepButton from "./NextStepButton";
+import { CreateProposalContext } from "@/context/createProposal.context";
 
-const TimelineForm = () => {
-  const [voteStart, setVoteStart] = useState(new Date());
-  const [voteDuration, setVoteDuration] = useState<string>("86400000");
-  const [mintStart, setMintStart] = useState(new Date());
-  const [mintDuration, setMintDuration] = useState<string>("86400000");
+interface OwnProps {
+  setActiveStep: React.Dispatch<React.SetStateAction<number>>;
+}
+
+const TimelineForm = ({ setActiveStep }: OwnProps) => {
+  const { state, dispatch } = useContext(CreateProposalContext);
+
+  const formValidation = () => {
+    if (!state.voteDuration || !state.voteStart) {
+      return true;
+    }
+    return false;
+  };
 
   return (
     <div className="w-full">
@@ -17,14 +27,14 @@ const TimelineForm = () => {
             Vote Start
           </label>
           <DateTimePicker
-            startDate={voteStart}
-            setStartDate={setVoteStart}
+            startDate={state.voteStart}
+            setStartDate={(date) => dispatch({ type: 'SET_VOTE_START', payload: date })}
             minDate={new Date()}
           />
           <CustomFormInput
             label="Vote Duration"
-            value={voteDuration}
-            onChange={(e) => setVoteDuration(e.target.value)}
+            value={state.voteDuration}
+            onChange={(e) => dispatch({ type: 'SET_VOTE_DURATION', payload: e.target.value })}
             formType="select"
             options={[
               { label: "1 DAY", value: "86400000" },
@@ -37,32 +47,12 @@ const TimelineForm = () => {
             ]}
           />
         </div>
-
-        <div className="flex w-fit flex-col">
-          <label className="font-mono text-sm font-semibold tracking-tight text-gray-200">
-            Vote Start
-          </label>
-          <DateTimePicker
-            startDate={mintStart}
-            setStartDate={setMintStart}
-            minDate={new Date()}
+        <NextStepButton
+            onClick={() => setActiveStep(3)}
+            disabled={formValidation()}
+            text="NEXT"
+            includeIcon
           />
-          <CustomFormInput
-            label="Mint Duration"
-            value={mintDuration}
-            onChange={(e) => setMintDuration(e.target.value)}
-            formType="select"
-            options={[
-              { label: "1 DAY", value: "86400000" },
-              { label: "2 DAYS", value: "172800000" },
-              { label: "3 DAYS", value: "259200000" },
-              { label: "4 DAYS", value: "345600000" },
-              { label: "5 DAYS", value: "432000000" },
-              { label: "6 DAYS", value: "518400000" },
-              { label: "1 WEEK", value: "604800000" },
-            ]}
-          />
-        </div>
         {/* <div className="mt-12 flex justify-between">
           <button>Previous</button>
           <button>Next</button>
