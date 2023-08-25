e6cc3fa488bf7c8e54c29185049d0ff1ed487ebf
rahmteen <ramtin.khoee@gmail.com>
2023-08-25 13:07:48 -0700

    wip api calls and app org

---
+++ app/_components/Header/Header.tsx
@@ -0,0 +1,18 @@
+import { Typography } from "@/common";
+
+const Header = () => {
+  return (
+    <>
+      <Typography
+        fontFamily="font-inter"
+        fontSize="text-5xl lg:text-8xl"
+        color="text-white"
+        fontWeight="font-bold"
+      >
+        VOTING FOR
+      </Typography>
+    </>
+  );
+};
+
+export default Header;
