6b70d9831eb2b3fd0b00712207c873f1c9e3d493
rahmteen <ramtin.khoee@gmail.com>
2023-08-29 12:08:25 -0700

    updated folder name

--- common/index.ts
+++ common/index.ts
@@ -1,4 +1,4 @@
-export { default as Typography } from "@/common/Typography/Typography";
+export { default as Typography } from "@/common/Text/Typography";
 export { default as Badge } from "@/common/Badge/Badge";
 export { default as Flex } from "@/common/Layout/Flex";
 export { default as Box } from "@/common/Layout/Box";
