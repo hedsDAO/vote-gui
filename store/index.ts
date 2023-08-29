6b921280d78436c9e2a7030706ee6ad294a7f465
cgreer011 <camgreer01@gmail.com>
2023-08-29 10:55:22 -0400

    create reducer for activeVote

--- store/index.ts
+++ store/index.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import proposal from "@/store/proposal";
+import activeVote from "./activeVote";
 
 export const store = configureStore({
   reducer: {
+    activeVote,
     proposal
   },
 });
