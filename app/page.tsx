e8ad15e7fd91a877cd27f8856aa9aa4313697fb0
cgreer011 <camgreer01@gmail.com>
2023-08-29 10:10:08 -0400

    ts-ignore server component warning

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <div className={styles.$homeLayoutStyles}>
       <Header />
+      {/* @ts-expect-error Server Component */}
       <Spaces />
     </div>
   );
