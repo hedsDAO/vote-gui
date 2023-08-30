438bf5abc9ea4838322f1fff73d8a87a6a70f416
rahmteen <ramtin.khoee@gmail.com>
2023-08-30 11:35:54 -0700

    merged changes from origin and refactored create proposal button

---
+++ components/buttons/CreateProposalButton/styles.ts
@@ -0,0 +1,11 @@
+import { ButtonProps } from "@chakra-ui/react";
+
+export const $createProposalButtonStyles: ButtonProps = {
+  display: "flex",
+  alignItems: "center",
+  gap: "2",
+  px: "4",
+  fontFamily: 'grotesk',
+  fontSize: 'sm',
+  textColor: 'black',
+};
