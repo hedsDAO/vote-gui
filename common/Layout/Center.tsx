150deb461d105e6a3b94d4e5219ca552b6225b22
rahmteen <ramtin.khoee@gmail.com>
2023-09-04 14:55:47 -0700

    fixed loading scren on card

--- common/Layout/Center.tsx
+++ common/Layout/Center.tsx
@@ -1,7 +1,7 @@
 "use client";
-import { Center as ChakraCenter, CenterProps } from "@chakra-ui/react";
+import { Center as ChakraCenter, CenterProps, ButtonProps } from "@chakra-ui/react";
 
-const Center = (props: CenterProps) => {
+const Center = (props: CenterProps | ButtonProps) => {
   return <ChakraCenter {...props} />;
 };
 
